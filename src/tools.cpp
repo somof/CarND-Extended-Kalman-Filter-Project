6329b2e95dbb3f0573270dae58ac3886c3d283b6
Hiroshi, Ichiki <udacity_github@somof.net>
2017-09-20 17:51:22 +0900

    confirm RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -44,6 +44,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//calculate the squared root
 	rmse = rmse.array().sqrt();
 
+    // cout << estimations.size() << " " << rmse << endl;
 	//return the result
 	return rmse;
 }
