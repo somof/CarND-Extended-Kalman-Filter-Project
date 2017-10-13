435f1b07aeb857a680afd78e416507bdbe35bb1a
somof <gituser@somof.net>
2017-10-14 08:27:34 +0900

    re indent

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -10,78 +10,78 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+   x_ = x_in;
+   P_ = P_in;
+   F_ = F_in;
+   H_ = H_in;
+   R_ = R_in;
+   Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  // predict the state
-  x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+   // predict the state
+   x_ = F_ * x_;
+   MatrixXd Ft = F_.transpose();
+   P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  // update the state by using Kalman Filter equations
+   // update the state by using Kalman Filter equations
 
-  // VectorXd y = z - H_ * x_;
-  VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred;
+   // VectorXd y = z - H_ * x_;
+   VectorXd z_pred = H_ * x_;
+   VectorXd y = z - z_pred;
 
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
+   MatrixXd Ht = H_.transpose();
+   MatrixXd S = H_ * P_ * Ht + R_;
+   MatrixXd Si = S.inverse();
 
-  // MatrixXd K =  P_ * Ht * Si;
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+   // MatrixXd K =  P_ * Ht * Si;
+   MatrixXd PHt = P_ * Ht;
+   MatrixXd K = PHt * Si;
 
-  //new state
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+   //new state
+   x_ = x_ + (K * y);
+   long x_size = x_.size();
+   MatrixXd I = MatrixXd::Identity(x_size, x_size);
+   P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  // update the state by using Extended Kalman Filter equations
+   // update the state by using Extended Kalman Filter equations
 
-  // Get predicted location in polar coords.
-  float px = x_(0);
-  float py = x_(1);
-  float vx = x_(2);
-  float vy = x_(3);
+   // Get predicted location in polar coords.
+   float px = x_(0);
+   float py = x_(1);
+   float vx = x_(2);
+   float vy = x_(3);
 
-  float rho = sqrtf(px * px + py * py);
-  float phi = atan2f(py, px);
-  float rho_dot = (px * vx + py * vy) / (rho + FLT_MIN);
-  // RMSE: 0.0973, 0.0855, 0.4513, 0.4399
+   float rho = sqrtf(px * px + py * py);
+   float phi = atan2f(py, px);
+   float rho_dot = (px * vx + py * vy) / (rho + FLT_MIN);
+   // RMSE: 0.0973, 0.0855, 0.4513, 0.4399
 
-  // from Forum: EKF gets off track
-  // https://discussions.udacity.com/t/ekf-gets-off-track/276122
-  float diff = z(1) - phi;
-  if (6.2 < diff) {
+   // from Forum: EKF gets off track
+   // https://discussions.udacity.com/t/ekf-gets-off-track/276122
+   float diff = z(1) - phi;
+   if (6.2 < diff) {
       phi += 2 * M_PI;
-  }
+   }
 
-  VectorXd z_pred(3);
-  z_pred << rho, phi, rho_dot; // H_ * x_;
+   VectorXd z_pred(3);
+   z_pred << rho, phi, rho_dot; // H_ * x_;
 
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
+   VectorXd y = z - z_pred;
+   MatrixXd Ht = H_.transpose();
+   MatrixXd S = H_ * P_ * Ht + R_;
+   MatrixXd Si = S.inverse();
 
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd K = PHt * Si;
+   MatrixXd PHt = P_ * Ht;
+   MatrixXd K = PHt * Si;
 
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-	P_ = (I - K * H_) * P_;
+   //new estimate
+   x_ = x_ + (K * y);
+   long x_size = x_.size();
+   MatrixXd I = MatrixXd::Identity(x_size, x_size);
+   P_ = (I - K * H_) * P_;
 }
